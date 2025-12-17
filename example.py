35d7f053cff822894594618d55e84d0b3e02519e
chris wren <chriswrendeveloper@gmail.com>
2025-12-17 20:32:51 +0000

    Data Structures

---
+++ example.py
@@ -0,0 +1,70 @@
+print("hello world")
+## Data Types
+
+# Integers - int
+
+# Floating Point - float
+
+# Strings - str
+
+# Lists - list
+
+# Dictionaries - dict
+
+# Tuples - tup
+
+# Sets - set
+
+# Booleans - bool
+
+
+## Aritmetic
+
+"""
+2 + 1
+2 - 1
+2 * 2
+3 / 2
+7 % 4 #modulo (remainder)
+2 ** 3 #power of
+(2 + 10) - 3
+"""
+
+## Variable Assignment
+
+a = 5
+a = 10
+a = a + a
+type(a) #int
+b = 2.0
+type(b) #float
+
+
+## Strings
+
+'hello'
+"Hello"
+'I\'m going out'
+
+#[start:stop:step]
+
+len("hello") #5
+
+mystring = "hello world"
+mystring[0] #first character
+mystring[-1] #last character
+
+## Slices
+
+mystring = "abcdefghjk"
+mystring[2:] #c onwards
+mystring[:2] #upto not including c
+mystring[3:6] #def
+mystring[::3] #jump steps of 3
+mystring[::-1] #reverse string
+
+## Immutability
+
+name = "Sam"
+name[0] = "H"
+
